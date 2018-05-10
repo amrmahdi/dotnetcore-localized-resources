8bba1610e77cfcb3bbf9a21dcd198b170d78c8e5
Amr Hanafi (MAHDI)) <amrh@microsoft.com>
2018-05-09 17:29:45 -0700

    Create a repro project for error demonastration

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace repro
+{
+    using repro.Resources;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
